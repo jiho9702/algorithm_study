6367e8661b52082ebaf9e575b94fcf41cf584b1b
이지호 <42643691+jiho9702@users.noreply.github.com>
2021-12-08 23:25:06 +0900

    백준 5635

--- 소스.cpp
+++ 소스.cpp
@@ -1,38 +1,29 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-int arr[100];
+
 
 int main() {
 
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	ios_base::sync_with_stdio(0); 
+	cin.tie(0);
+	cout.tie(0);
 
-	int n, k;
-	cin >> n >> k;
+	int n;
+	cin >> n;
+	
+	vector <pair< pair<int, int>, pair<int, string>>> name(n);
 
 	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
+		cin >> name[i].second.second >> name[i].second.first >> name[i].first.second >> name[i].first.first;
 	}
 
-	int sum[100] = {};
+	sort(name.begin(), name.end());
 
-	for (int i = 0; i <= n-k; i++) {
-		for (int j = i; j < i + k; j++) {
-			sum[i] += arr[j];
-		}
-	}
-
-	int result;
-	result = sum[0];
-	for (int i = 1; i <= n - k; i++) {
-		if (result < sum[i]) {
-			result = sum[i];
-		}
-	}
-
-	cout << result;
+	cout << name[n-1].second.second << "\n";
+	cout << name[0].second.second << "\n";
 
 }
