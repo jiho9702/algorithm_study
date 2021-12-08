54bfc3c934a9ac461126c094d8cdf50d44fbd328
이지호 <42643691+jiho9702@users.noreply.github.com>
2021-12-08 23:29:28 +0900

    백준 5635

---
+++ 백준5635.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+int main() {
+
+	ios_base::sync_with_stdio(0); 
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+	
+	vector <pair< pair<int, int>, pair<int, string>>> name(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> name[i].second.second >> name[i].second.first >> name[i].first.second >> name[i].first.first;
+	}
+
+	sort(name.begin(), name.end());
+
+	cout << name[n-1].second.second << "\n";
+	cout << name[0].second.second << "\n";
+
+}
