ce7cfcf8f72b866933eccc5995da67827bbe4440
이지호 <42643691+jiho9702@users.noreply.github.com>
2022-01-29 18:08:11 +0900

    Add files via upload

---
+++ 뜨거운붕어빵.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+
+#fish_bread = []
+# for _ in range(n):
+#     fish_bread.append(input())
+
+# print(fish_bread)
+    
+# for index in fish_bread:
+#     print(index[::-1], end = '')
+
+for _ in range(n):
+    fish_bread = input()
+    print(fish_bread[::-1])
